2bea5c5def7c48f2fd4d75086574c5d0a5bb7120
Jerick-andh <jerickandreiherrera@gmail.com>
2024-11-06 00:10:48 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -3,21 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HomePage</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles.css">
+    <title>Hunger Relief Platform</title>
+    <link rel="stylesheet" href="sheet.css">
 </head>
 <body>
-    <nav>
-        <ul>
-            <li> <a href="index.php">Home</a></li>
-            <li>Search Food</li>
-            <li>Donate</li>
-            <li>Request</li>
-            <li>Dashboard</li>
-            <li>Profile</li>
-        </ul>
-    </nav>
-    
+    <div class="header">
+        <div class="logo"><img src = lablab.jpg></div>
+        <nav class="nav-menu">
+            <a href="#">Home</a>
+            <a href="#">Search Food</a>
+            <a href="#">Donate</a>
+            <a href="#">Request</a>
+            <a href="#">Dashboard</a>
+            <a href="#">Profile</a>
+            <a href="#">Logout</a>
+        </nav>
+    </div>
+    <div class="main-content">
+        <h1>Hunger Relief Platform</h1>
+        <p>A Surplus Food Donation and Distribution Management System</p>
+        <div class="background-image">
+            <div class="cta-section">
+                <div class="cta-image"><img src = lablab.jpg alt = "Donate"> </div>
+                <p>"Your support can change lives—become a donor today and help make a lasting impact!"</p>
+                <button class="donor-button">Become a donor</button>
+            </div>
+        </div>
+    </div>
 </body>
-</html>
+</html
