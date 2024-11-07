ed568dc097a3f99af72be6987e1a492b499dc735
Angelito Gonzales <23-36439@g.batstate-u.edu.ph>
2024-11-07 22:58:17 +0800

    add interfaces for user

--- index.php
+++ index.php
@@ -1,23 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HomePage</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-    <nav>
-        <ul>
-            <li> <a href="index.php">Home</a></li>
-            <li>Search Food</li>
-            <li>Donate</li>
-            <li>Request</li>
-            <li>Dashboard</li>
-            <li>Profile</li>
-        </ul>
-    </nav>
-    
+<?php
+include "layout/header.php";
+?>
+
+<div style="background-image: url('images/backgroundhomepage.jpg'); background-size: cover; background-position: center; height: 100vh;">
+  <div class="container text-white py-5">
+    <div class="row align-items-center g-5">
+      <div class="col-md-6">
+      
+        <h1 class="mb-5" style="font-size: 5rem;"><strong>Hunger Relief Platform</strong></h1>
+        <p>
+        A Surplus Food Donation and Distribution Management System
+        </p>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+      </div>
+      <div class="col-md-6 text-center">
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php
+include "layout/footer.php";
+?>
+  
 </body>
 </html>
