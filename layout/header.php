ee002b89b6ecd40e5fa5f88fa5c55489225fa9e5
Angelito Gonzales <23-36439@g.batstate-u.edu.ph>
2024-11-11 21:05:32 +0800

    Modified Header added sweetalert boostrap

--- layout/header.php
+++ layout/header.php
@@ -1,21 +1,23 @@
 <?php
 session_start();
-
-$authenticated = false;
-if (isset($_SESSION["email"])) {
-  $authenticated = true;
-}
+$authenticated = isset($_SESSION["email"]);
 ?>
 
 <!doctype html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Hunger Relief Platform</title>
     <link rel="icon" href="images/logo.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  </head>
+     <!-- Bootstrap Bundle with Popper.js -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN6jIeHz" crossorigin="anonymous"></script>
+    <!-- sweetalert --> 
+    <link rel="stylesheet" href="sweetalert2.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+</head>
   <body>
 
   <nav class="navbar navbar-expand-lg" style="background-color: #FFFFE0; border-bottom: 1px solid #ddd; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
@@ -37,7 +39,7 @@ if (isset($_SESSION["email"])) {
                         <a class="nav-link text-dark" href="#">Search Food</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link text-dark" href="#">Donate</a>
+                        <a class="nav-link text-dark" href="donate.php">Donate</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link text-dark" href="#">Request</a>
@@ -45,39 +47,25 @@ if (isset($_SESSION["email"])) {
                     <li class="nav-item">
                         <a class="nav-link text-dark" href="#">Dashboard</a>
                     </li>
-                <?php endif; ?>
-            </ul>
-
-            <?php if ($authenticated): ?>
-                <ul class="navbar-nav">
-                    <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle text-dark" href="index.php" id="profileDropdown" role="button" data-bs-toggle="dropdown" data-bs-target="#dropdown-menu" aria-expanded="false">
-                                Your Profile
-                            </a>
-                        <ul class="dropdown-menu" aria-labelledby="profileDropdown">
-                            <li><a class="dropdown-item" href="profile.php">Profile</a></li>
-                            <li><hr class="dropdown-divider"></li>
-                            <li><a class="dropdown-item" href="logout.php">Logout</a></li>
-                        </ul>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" href="profile.php">Profile</a>
                     </li>
-                </ul>
-            <?php else: ?>
-                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="logout.php" class="btn btn-outline-danger ms-2">Logout</a>
+                    </li>
+                <?php else: ?>
                     <li class="nav-item">
                         <a href="signin.php" class="btn btn-outline-primary me-2">Register</a>
                     </li>
                     <li class="nav-item">
                         <a href="login.php" class="btn btn-primary">Login</a>
                     </li>
-                </ul>
-            <?php endif; ?>
+                <?php endif; ?>
+            </ul>
         </div>
     </div>
   </nav>
   
-  <!-- Bootstrap Bundle with Popper.js -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN6jIeHz" crossorigin="anonymous"></script>
-
+ 
 </body>
 </html>
-                
