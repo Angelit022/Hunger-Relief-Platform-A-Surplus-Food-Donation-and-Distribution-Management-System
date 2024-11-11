5b922723678bcfa1c33a7704205ca184cd0d4244
Angelito Gonzales <23-36439@g.batstate-u.edu.ph>
2024-11-11 21:09:35 +0800

    modified connection using class to make it reusable

--- db_connection.php
+++ db_connection.php
@@ -1,18 +1,26 @@
 <?php
-function getDatabaseConnection() {
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "hunger_relief_db";
-    
-    // Create connection
-    $connection = new mysqli($servername, $username, $password, $database);
-    
-    // Check connection
-    if ($connection->connect_error) {
-        die("Error: Failed to connect to MySQL - " . $connection->connect_error);
+class Database {
+    private $host = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $database = "hunger_relief_db";
+    private $connection;
+
+    public function getConnection() {
+        if ($this->connection === null) {
+            $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
+            if ($this->connection->connect_error) {
+                die("Error: Failed to connect to MySQL - " . $this->connection->connect_error);
+            }
+        }
+        return $this->connection;
     }
-    
-    return $connection;
+
+    public function closeConnection() {
+        if ($this->connection !== null) {
+            $this->connection->close();
+            $this->connection = null;
+        }
+    }
 }
 ?>
